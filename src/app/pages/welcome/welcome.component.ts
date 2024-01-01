7a719801c5e0162d307bd50126918a56840cdbd7
Akil <akil.siddique@postex.pk>
2024-01-01 18:22:01 +0500

    steper

--- src/app/pages/welcome/welcome.component.ts
+++ src/app/pages/welcome/welcome.component.ts
@@ -1,5 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { NzUploadFile } from 'ng-zorro-antd/upload';
 
+const getBase64 = (file: File): Promise<string | ArrayBuffer | null> =>
+  new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => resolve(reader.result);
+    reader.onerror = error => reject(error);
+  })
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
@@ -12,4 +20,5 @@ export class WelcomeComponent implements OnInit {
   ngOnInit() {
   }
 
+  
 }
--- src/app/pages/welcome/welcome.module.ts
+++ src/app/pages/welcome/welcome.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 
 import { WelcomeRoutingModule } from './welcome-routing.module';
+import { NzBreadCrumbModule } from 'ng-zorro-antd/breadcrumb';
+
 
 import { WelcomeComponent } from './welcome.component';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzStepsModule } from 'ng-zorro-antd/steps';
+
+
 
 
 @NgModule({
-  imports: [WelcomeRoutingModule],
+  imports: [WelcomeRoutingModule, NzButtonModule, NzStepsModule, NzBreadCrumbModule
+],
   declarations: [WelcomeComponent],
   exports: [WelcomeComponent]
 })
